8898f7ece7e326e1998d27d431cf0326f292237d
NovoxMx <A00835132@tec.mx>
2024-04-19 16:47:39 -0700

    add hover

--- src/app/(dashboard)/partnershipsedi/page.tsx
+++ src/app/(dashboard)/partnershipsedi/page.tsx
@@ -7,7 +7,7 @@ export default function PartnershipTable() {
     return (
         <div className="bg-white overflow-hidden mx-auto w-5/6 h-5/6 mt-10 border border-gray-300 rounded-lg shadow-lg ">
             <div className="flex justify-end mt-8 mr-4">
-                <button className="bg-black text-white px-4 py-2 flex items-center rounded-lg ">
+                <button className="bg-black text-white px-4 py-2 flex items-center rounded-lg hover:bg-white hover:text-black hover:border-black border hover:border-solid">
                     Download PO Test <MdOutlineFileUpload className="ml-2" />
                 </button>
             </div>
